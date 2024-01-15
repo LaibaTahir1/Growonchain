7fb9e6ac9c4621d5befbf10d52580e8c89b30420
MuhammadLuqmans <musa60209@gmail.com>
2024-01-15 23:45:36 +0500

    add map

--- src/App.js
+++ src/App.js
@@ -22,34 +22,32 @@ import Boxes from './Components/Boxes';
 
 function App() {
   return (
-    <div className='bg-black'>
-    <Top/>
-    <Intro/>
-    <Progress/>
-    <Returning/>
-    <DailyTasks/>
-    <Newto/>
-    <Communities/>
-    <Infinity/>
-    <LastNew/>
-    <Ending/>
-    </div>
-    // <div >
-    //   <Layout >
-    //     <Header />
-    //     <div className='max-w-[1240px] mx-auto p-2'>
-    //     <Boxes/>
-    //     <Description />
-    //     <Feature />
-    //     <Journey />
-    //     <Benefit />
-    //     <Figma />
-    //     <Faq />
-    //     </div>
-    //   </Layout>
+    // <div className='bg-black'>
+    // <Top/>
+    // <Intro/>
+    // <Progress/>
+    // <Returning/>
+    // <DailyTasks/>
+    // <Newto/>
+    // <Communities/>
+    // <Infinity/>
+    // <LastNew/>
+    // <Ending/>
     // </div>
-   
-
+    <div >
+      <Layout >
+        <Header />
+        <div className='max-w-[1240px] mx-auto p-2'>
+        <Boxes/>
+        <Description />
+        <Feature />
+        <Journey />
+        <Benefit />
+        <Figma />
+        <Faq />
+        </div>
+      </Layout>
+    </div>
   );
 }
 export default App;
--- src/Components/Feature.js
+++ src/Components/Feature.js
@@ -1,70 +1,77 @@
-import React from 'react'
+import React from 'react';
+
+const KEY_FEATURE = [
+  {
+    id: 1,
+    title: 'Weekly LIVE Session',
+    image: '/assets/images/Calendar.png',
+  },
+  {
+    id: 2,
+    title: 'Make Real Friends',
+    image: '/assets/images/heart11.png',
+  },
+  {
+    id: 3,
+    title: 'Real Assignments',
+    image: '/assets/images/star11.png',
+  },
+  {
+    id: 4,
+    title: 'Mentor Based Learning',
+    image: '/assets/images/badminton-line.png',
+  },
+  {
+    id: 5,
+    title: 'Get Shit Done',
+    image: '/assets/images/shuttle1.png',
+  },
+  {
+    id: 6,
+    title: 'Exclusive Guest Sessions',
+    image: '/assets/images/1sparkle.png',
+  },
+  {
+    id: 7,
+    title: 'Real Stories',
+    image: '/assets/images/coin.png',
+  },
+  {
+    id: 8,
+    title: 'Build Your Knowledge',
+    image: '/assets/images/sign.png',
+  },
+  {
+    id: 9,
+    title: 'Lifetime Network',
+    image: '/assets/images/ok.png',
+  },
+];
 
 function Feature() {
   return (
     <>
-    <div className=' text-white pb-20'>
-        <div className='text-center p-10'>
-        <div className=' bg-[#2C3B87]   max-w-[800px] mx-auto w-full  rounded-2xl '>
-                <h1 className=' text-white  text-start  pl-10 pt-10 text-[25px] md:text-[54px] font-semibold'>Key Features</h1>
-                <div className=' flex gap-4  mt-[60px] pl-10 pr-10 justify-center'>
-                <div className='w-80 text-x rounded-xl lh-32 p-3 font-thin md:font-bold text-start bg-[#4860DE]'>
-                    <img className='pt-3 pl-3' src="./assets/images/Calendar.png" alt=""></img>
-                    <div className='leading-[22px] text-xl font-semibold pl-3'><p  className='mt-4 '>Weekly LIVE </p>
-                    <p  className=' '>Session</p></div>
-                </div>
-                <div className='w-80 text-x rounded-xl   p-3 font-bold text-start bg-[#4860DE]'>
-                <img className='pt-3 pl-3' src="./assets/images/heart11.png" alt=""></img>
-                   <div  className=' text-xl font-bold pl-3 leading-[22px]'> <p  className='mt-4 '> Make Real </p>
-                    <p > Friends</p></div>
-                </div>
-                <div className='w-80 text-x rounded-xl  lh-32  p-3 font-bold text-start bg-[#4860DE]'>
-                <img className='pt-3 pl-3' src="./assets/images/star11.png" alt="" />
-                    <div className=' text-xl font-bold pl-3 leading-[22px]'><p  className='mt-4'> Real</p>
-                    <p  >Assignments</p></div>
-                </div>
-                </div>
-                <div className='flex  mt-4 h-48  pl-10 pr-10  gap-4 justify-center'>
-                <div className='w-80  text-x rounded-xl lh-32  font-bold text-start bg-[#4860DE]'>
-                <img className='pt-3 pl-3' src="./assets/images/badminton-line.png" alt=""></img>
-                   <div className='mt-3 text-xl font-bold pl-3 leading-[22px]'> <p  className='mt-4'> Mentor Based</p>
-                    <p  >Learning</p></div>
-                </div>
-                <div className='w-80   text-x rounded-xl lh-32  font-bold text-start bg-[#4860DE]'>
-                <img className='pt-3 pl-3' src="./assets/images/shuttle1.png" alt=""></img>
-                    <div  className='text-xl font-bold pl-3 leading-[22px]'><p  className='mt-4'> Get Shit </p>
-                    <p > Done</p></div>
-                </div>
-                <div  className='w-80  text-x rounded-xl lh-32  font-bold text-start bg-[#4860DE]'>
-                <img className='pt-3 pl-3' src="./assets/images/1sparkle.png" alt=""></img>
-                    <div  className=' text-xl font-bold pl-3 leading-[22px]'><p  className='mt-4'> Exclusive Guest </p>
-                    <p >  Sessions</p></div>
-                </div>
-                </div>
-                <div className='flex h-48  pl-10 pr-10 pb-10   mt-4 gap-4 justify-center'>
-                <div className='w-80  text-x rounded-xl lh-32  font-bold text-start bg-[#4860DE]'>
-                <img className='pt-3 pl-3' src="./assets/images/coin.png" alt=""></img>
-                    <div className=' text-xl font-bold pl-3 leading-[22px]'><p  className='mt-4'>Real</p>
-                    <p  > Stories</p></div>
-                </div >
-                <div className='w-80  text-x rounded-xl lh-32  font-bold text-start bg-[#4860DE]'>
-                <img className='pt-3 pl-3' src="./assets/images/sign.png" alt=""></img>
-                    <div className='text-xl font-bold pl-3 leading-[22px]' ><p  className='mt-4'>Build Your</p>
-                    <p  > Knowledge</p></div>
-                </div>
-                <div className='w-80   text-x rounded-xl lh-32  font-bold text-start bg-[#4860DE]'>
-                <img className='pt-3 pl-3' src="./assets/images/ok.png" alt=""></img>
-                    <div  className=' text-xl font-bold pl-3 leading-[22px]'><p  className='mt-4'>Lifetime </p>
-                    <p > Network</p></div>
-                </div>
-                </div>
-              </div>
-
+      <div className=' text-white pb-20'>
+        <div className='text-center'>
+          <div className=' bg-[#2C3B87]  p-10 max-w-[800px] mx-auto w-full  rounded-2xl '>
+            <h1 className=' text-white  text-start  text-[25px] md:text-[54px] font-semibold'>Key Features</h1>
+            <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4  mt-[60px]  justify-center'>
+              {KEY_FEATURE.map((item, idx) => {
+                return (
+                  <div key={`${item.id}___${idx}`} className=' text-x rounded-xl lh-32 p-3 font-thin md:font-bold text-start bg-[#4860DE]'>
+                    <img className='pt-3 pl-3' src={item.image} alt='' />
+                    <div className='leading-[22px] text-xl font-semibold pl-3'>
+                      <p className='mt-4 '>{item.title}</p>
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
         </div>
-
-    </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default Feature
+export default Feature;
